432376562be0f7fdb1415bc68a863d9f8b6f6678
Nasrudin Salim <nasrudin.salim.suden@gmail.com>
2018-01-24 10:49:54 +0800

    clean

--- video_run.py
+++ video_run.py
@@ -1,20 +1,27 @@
-#Author: Nasrudin Bin Salim
-#Copyright (C) Nasrudin B Salim 2018
-from darkflow.cli import cliHandler as yolo
+# Author: Nasrudin Bin Salim
+# Copyright (C) Nasrudin B Salim 2018
+from util.parser import yolo_run
 
 
-#location of weights
-model_cfg = 'yolo-voc.cfg'
-model_weight = 'weights/yolo-voc.weights'
-video_feed ='camera'
+# Parameters here
+
+## Path to model and weight
+model_cfg = 'weights/yolo-voc.cfg'
+model_weight = 'weights/yolov2.weights'
+
+# Bools
+use_gpu = True
+save_results = True
+
 
 def main():
-    pass
+    video_file = input("Path to video file: ")
+    yolo_run(model=model_cfg,weights=model_weight,video_file_or_camera=video_file,use_gpu=use_gpu,save_results=save_results)
 
 
 
 if __name__ == '__main__':
-    pass
+    main()
 
 
 
