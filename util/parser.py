c91bc23bdd37e1c20b7aa27d67d3dd149e8186b8
Nasrudin Salim <Nasrudin.Salim.Suden@gmail.com>
2018-10-26 21:43:05 +0800

    Created an easy yolo wrapper facade api function

--- util/parser.py
+++ util/parser.py
@@ -1,21 +1,22 @@
 from util.parser.defaults import argHandler
-
+from config import MODEL_CFG_FILE_PATH, MODEL_WEIGHT
 from collections import namedtuple
+import os
+from darkflow.darkflow.net.build import TFNet
 
 UserRunInput = namedtuple("UserRunInputs",
                           ['use_gpu', 'use_video', 'use_camera', 'video_path', 'save', 'save_video_fp'])
 UserTrainInput = namedtuple("UserTrainInputs", ['use_gpu', 'epoch', 'save', 'learning_rate', 'batch'])
 
-from config import MODEL_CFG_FILE_PATH, MODEL_WEIGHT
-
 
 class YoloArgs(argHandler):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, user_inputs, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Need to set the model and weight paths
         self['model'] = MODEL_CFG_FILE_PATH
         self['load'] = MODEL_WEIGHT
+        self.take_user_inputs(user_inputs)
 
     def take_user_inputs(self, input_dict: namedtuple):
 
@@ -32,10 +33,11 @@ class YoloArgs(argHandler):
         elif tuple_input.use_camera:
             self['demo'] = 'camera'
         if tuple_input.save:
-            # FIXME: We actually want to specify where to save the video to
+            # FIXME: We actually want to specify where to save the video to, currently not supported
             self['saveVideo'] = True
 
     def _parse_train_inputs(self, tuple_input: UserTrainInput):
+        self['train'] = True  # Train it
         if tuple_input.use_gpu:
             self['gpu'] = 1.0
         self['lr'] = tuple_input.learning_rate
@@ -44,8 +46,47 @@ class YoloArgs(argHandler):
         self['save'] = tuple_input.save
 
 
-# execute clihandler via python
-def _execute_yolo(commandstring):
-    command = [""] + commandstring.split(" ")
-    print("executing command", command)
-    cliHandler(command)
+def _get_dir(dirs):
+    for d in dirs:
+        this = os.path.abspath(os.path.join(os.path.curdir, d))
+        if not os.path.exists(this): os.makedirs(this)
+
+
+def easy_yolo(user_input):
+    """
+    yolo wrapper, facade
+    """
+
+    instructions = YoloArgs(user_input)
+
+    requiredDirectories = [instructions.imgdir, instructions.binary, instructions.backup,
+                           os.path.join(instructions.imgdir, 'out')]
+    if instructions.summary:
+        requiredDirectories.append(instructions.summary)
+
+    _get_dir(requiredDirectories)
+
+    # fix FLAGS.load to appropriate type
+    try:
+        instructions.load = int(instructions.load)
+    except:
+        pass
+
+    tfnet = TFNet(instructions)
+
+    if instructions.demo:
+        tfnet.camera()
+        exit('Demo stopped, exit.')
+
+    if tfnet.train:
+        print('Enter training ...')
+        tfnet.train()
+        if not instructions.savepb:
+            exit('Training finished, exit.')
+
+    if instructions.savepb:
+        print('Rebuild a constant version ...')
+        tfnet.savepb()
+        exit('Done')
+
+    tfnet.predict()
