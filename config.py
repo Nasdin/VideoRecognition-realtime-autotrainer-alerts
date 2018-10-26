3bc3bb9673d1b4e177dc733a4e9d5ed58aca873c
Nasrudin Salim <Nasrudin.Salim.Suden@gmail.com>
2018-10-26 22:26:40 +0800

    Change config to allow yolov3 model path

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Sets as the default path for the CLI
 
 # YOLO Model
-MODEL_CFG_FILE_PATH = "weights/yolo-voc.cfg"
-MODEL_WEIGHT = "weights/yolov2.weights"
+MODEL_CFG_FILE_PATH = "data/yolov3.cfg"
+MODEL_WEIGHT = "data/yolov3.weights"
 USE_GPU = False
 SAVE_RESULTS = False
-DEFAULT_VIDEO_SAVE_ADDRESS = "video.avi"
+DEFAULT_VIDEO_SAVE_ADDRESS = "video.avi"  # Currently no work and needs fixing
 TRAIN_RATIO = 0.7
 
 # WebScraper
